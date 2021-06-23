25d24223ac46d0019e2a9ac49d116c2242305295
Wolfleader <james.morgan@iinet.net.au>
2021-06-23 16:59:40 +0800

    basic chunks working

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    public int chunkSize;
+    
+    private Mesh _mesh;
+    
+    private Vector3[] _vertices;
+    private int[] _triangles;
+    
+    void Start()
+    {
+        _mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = _mesh;
+        
+        CreateShape();
+        UpdateMesh();
+        
+        GetComponent<MeshCollider>().sharedMesh = _mesh;
+    }
+    
+    void CreateShape()
+    {
+        _vertices = new Vector3[(chunkSize + 1) * (chunkSize + 1)];
+
+
+        for (int i = 0, z = 0; z <= chunkSize; z++)
+        {
+            for (int x = 0; x <= chunkSize; x++)
+            {
+                // float y = Mathf.PerlinNoise(x * perlinNoiseScale + perlinNoiseOffsetX,
+                //     z * perlinNoiseScale + perlinNoiseOffsetY) * heightScale;
+                
+                _vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        _triangles = new int[chunkSize * chunkSize * 6];
+        int vert = 0, tris = 0;
+        for (int z = 0; z < chunkSize; z++)
+        {
+            for (int x = 0; x < chunkSize; x++)
+            {
+                _triangles[tris + 0] = vert + 0;
+                _triangles[tris + 1] = vert + chunkSize + 1;
+                _triangles[tris + 2] = vert + 1;
+                _triangles[tris + 3] = vert + 1;
+                _triangles[tris + 4] = vert + chunkSize + 1;
+                _triangles[tris + 5] = vert + chunkSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+           
+            vert++;
+        }
+    }
+
+    void UpdateMesh()
+    {
+        _mesh.Clear();
+
+        _mesh.vertices = _vertices;
+        _mesh.triangles = _triangles;
+
+        _mesh.RecalculateNormals();
+    }
+    
+}
--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -5,19 +5,65 @@ using UnityEngine;
 public class WorldManager : MonoBehaviour
 {
     [SerializeField] private int worldSeed = 42069;
+    [SerializeField] private GameObject player;
 
-    [SerializeField] float perlinOffset;
+    [SerializeField] private int chunkSize = 128;
+    [SerializeField] private int loadedGridSize = 3;
+
+    [SerializeField] private Material grassMat;
+
+    private float _perlinOffset;
     
     // Start is called before the first frame update
     void Start()
     {
         Random.InitState(worldSeed);
 
-        perlinOffset = Random.value;
+        _perlinOffset = Random.value;
+
+        GenerateChunks();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void GenerateChunks()
+    {
+        // create grid of chunks
+        int totalChunks = loadedGridSize * loadedGridSize;
+        
+        // get middle chunk which is where player should be
+        int midChunk = (int)((float)totalChunks / 2 + 0.5);
+        
+        // create chunks from bottom left  going left to right
+
+        // loop over row
+        for (int row = 0, currentChunk = 1; row < loadedGridSize; row++)
+        {
+            // loop over col
+            for (int col = 0; col < loadedGridSize; col++, currentChunk++)
+            {
+                if (currentChunk == midChunk)
+                {
+                    // Debug.Log("Mid Chunk");
+                }
+                //Debug.Log($"Row: {row} at col {col}, current chunk = {currentChunk}");
+                
+                CreateChunk(row, col,chunkSize * col, chunkSize * row);
+            }
+        }
+
+    }
+
+    void CreateChunk(int row, int col, int x, int z)
+    {
+        GameObject chunk = new GameObject($"Chunk {row}x{col}", typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider), typeof(ChunkManager));
+        chunk.GetComponent<ChunkManager>().chunkSize = chunkSize;
+        
+        chunk.transform.localPosition = new Vector3(x, 0, z);
+
+        chunk.GetComponent<MeshRenderer>().materials[0] = grassMat;
     }
 }
